7372531b8182d4dbd4648d0239b962d3a17fe6bd
alanfrancis442 <--global>
2024-02-12 20:24:29 +0530

    made change

--- app/components/tech/TechMain.jsx
+++ app/components/tech/TechMain.jsx
@@ -1,7 +1,9 @@
+import Main from "./gallery/page";
 const TechMain = () => {
   return (
     <div className="min-h-screen bg-tech-bg">
-      <h2>Tech</h2>
+      {/* <h2>Tech</h2> */}
+      <Main />
     </div>
   );
 };
--- app/components/tech/gallery/page.jsx
+++ app/components/tech/gallery/page.jsx
@@ -1,9 +1,45 @@
+import Picture from "./picture";
 function Main() {
-    return ( 
-        <>
-        hello
-        </>
-     );
+  return (
+    <>
+      <div className=" border-2 border-black min-h-screen">
+        <div className="px-20 border-black">
+          <div className="top flex items-center justify-between">
+            <div className="flex">
+              <div className="img">
+                <img
+                  className="rounded-full w-[10rem] h-[10rem]"
+                  src="./image 1.png"
+                  alt="imh"
+                />
+              </div>
+              <div className="img">
+                <div className="flex justify-center items-center  rounded-full -translate-x-10 border-[0.5px] border-black bg-[rgba(0,0,0,0.3)]
+                backdrop-blur-sm
+                ">
+                  <h1 className="capitalize h-[10rem] w-[10rem] flex justify-center items-center">
+                    gallery
+                  </h1>
+                </div>
+              </div>
+            </div>
+              <div className="text-6xl">
+                <p>More than 100</p>
+                <p>events to conquer</p>
+              </div>
+              <div className="bg-[rgba(67,88,86,1)] w-[25%] h-1"></div>
+              <div className="h-[10vh] w-[10%] bg-red-500 relative">
+                <img className="" src="./Group 39.png" alt="" />
+                  <div className="absolute top-[50%] left-[50%] -translate-x-[50%] text-6xl">
+                02
+                </div>
+              </div>
+          </div>
+        </div>
+      <Picture />   
+      </div>
+    </>
+  );
 }
 
 export default Main;
