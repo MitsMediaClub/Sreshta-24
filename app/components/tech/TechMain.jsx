44e0061465c736d44ce6ce0a99e8fcc64786dbbc
Akshhay <=>
2024-02-13 21:48:08 +0530

    svg cursor (havent added the blurry backgorund)

--- app/components/tech/TechMain.jsx
+++ app/components/tech/TechMain.jsx
@@ -1,9 +1,11 @@
+import FloatButton from "./FloatButton";
 import { Landing } from "./landing/Landing";
 
 const TechMain = () => {
   return (
     <div className="min-h-screen bg-tech-bg">
       <Landing />
+      <FloatButton />
     </div>
   );
 };
