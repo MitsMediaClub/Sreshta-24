42531230ef2af2a3eb48abd0a5d7772e50220ce1
Vaishakh S Nair <vaishakhsnair2004@gmail.com>
2024-02-17 18:17:05 +0530

    added student coucil leads

--- app/about/About.jsx
+++ app/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import "./About.css"
+//import the json file
+import data from './StudentCouncil.json';
+
 
 export default function AboutPage() {
   return (
@@ -12,13 +15,37 @@ export default function AboutPage() {
       <p className='our-team-main-text'>OUR TEAM</p>
       <p className='our-team-subtext'>The Heroes, the Legends, the Myths</p>
       <div className='background-image gradient-overlay'></div>
+      <StudentCouncil data={data}/>
     </div>
   );
 };
 
 
 
-function StudentCouncil(){
+function StudentCouncil({data}){
+  data.Leads.map((lead) => {
+    console.log(lead)
+  })
+  return (
+    <div className='members-container'>
+      <p className='subsection-title'>STUDENT COUNCIL</p>
+      <div className='student-council-leads-container'>
+        {data.Leads.map((lead,index) => {
+          return (
+            <div className='student-council-leads' key={index}>
+              <img className='lead-image' src={lead.image} alt={lead.name} />
+              <div className='lead-details'>
+                <p className='lead-name'>{lead.name}</p>
+                <p className='lead-designation'>{lead.designation}</p>
+              </div>
+              
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+
 
 }
 
