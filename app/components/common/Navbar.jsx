cbd9da87b23f794dfeb1940c1d16ea9008718269
Nitin <nitinkm320@gmail.com>
2024-02-17 20:22:56 +0530

    navbar-done

--- app/components/common/Navbar.jsx
+++ app/components/common/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
     document.getElementById("nav-part1").style.height = "10vh";
     document.getElementById("nav-items-main").style.width = "100vw";
     document.getElementById("nav-items-main").style.height = "90vh";
+    document.getElementById("nav-items-main").style.animationName = "nav";
+    document.getElementById("nav-items-main").style.animationDuration = "3s";
     document.getElementById("nav-items-main").style.backgroundColor = "#DAEBEC";
     document.getElementById("nav-burger").style.display = "none";
     document.getElementById("nav-cross").style.display = "flex";
@@ -34,7 +36,6 @@ const Navbar = () => {
         <svg onClick={hamburger} className="nav-burger" id="nav-burger" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
         <svg onClick={navClose} className="nav-cross" id="nav-cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
       </div>
-
       <div className="nav-items-main" id="nav-items-main">
         <div className="nav-items-main1">
           <span className="nav-items">
