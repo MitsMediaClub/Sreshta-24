50b5cb0679b1529133f8cbf610d3a973cd5032c5
bamalekh <bamalekh@gmail.com>
2024-02-20 08:08:42 +0000

    Fonts Changed All Done

--- app/components/tech/faq/Faq.jsx
+++ app/components/tech/faq/Faq.jsx
@@ -90,6 +90,7 @@ export default function Faq() {
         </div>
       </div>
       <section className="section location">
+        <br/><br/>
         <h2 className="section-title">How to get here?</h2>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125748.46657650663!2d76.25601409726565!3d9.963914800000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0874de563bc58b%3A0xde7ecbfa110cfbda!2sMuthoot%20Institute%20of%20Technology%20%26%20Science!5e0!3m2!1sen!2sin!4v1708221925095!5m2!1sen!2sin"  loading="eager" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </section>
