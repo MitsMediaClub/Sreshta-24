ccd3d7b802a31140d02e51766f8eab8e592fdf2f
alanfrancis442 <--global>
2024-02-21 03:15:24 +0530

    completed

--- app/components/tech/gallery/top.jsx
+++ app/components/tech/gallery/top.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import React from "react";
+import styles from './styles.module.css'
 
 const Top = () => {
   return (
-    // <div>Top</div>
     <>
       <div className="flex justify-between items-center gap-10 mobile:gap-1 p-20 mobile:p-4 mh:gap-4 mh:p-4">
             <Image
@@ -13,7 +13,7 @@ const Top = () => {
               alt="Meh"
               className="w-56 mobile:w-28"
             />
-            <h1 className="dog text-6xl mh:text-3xl mobile:text-xs text-tech-primary">
+            <h1 className={`${styles.dg_text} text-6xl mh:text-3xl mobile:text-xs text-tech-primary`}>
               More than 100 <br /> events to conquer
             </h1>
             <hr className="w-1/2 mh:w-1/4 mobile:w-1/6 h-1 bg-tech-primary" />
