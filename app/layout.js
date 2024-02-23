24fe643df17441523a06238fc387c75bcbc86d34
Glitchyi <advaithnarayanan8@gmail.com>
2024-02-23 12:43:39 +0530

    eeh

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,6 @@ export const metadata = {
   description: "MITS Annual Tech Fest",
   description: "Muthoot Institute Of Technology and Science presents SHRESHTA'24,enriched with the essence of culture and technology. The techno-cultural fest fosters the best of everything. Together we learn, create and innovate.",
   keywords: "arts culture heritage themeshows vibrant fun learn innovate create enhance college gaming hub space info expo artificial intelligence exhibition music entertainment dance games zest inquistive",
-  viewport:"width=device-width, initial-scale=1.0",
   image: "https://i.imgur.com/F3WuV0L.png",
 };
 
