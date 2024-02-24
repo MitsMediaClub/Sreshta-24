97427cea804f540a49901bd8e2e706e80a3117f8
Glitchyi <advaithnarayanan8@gmail.com>
2024-02-24 18:39:30 +0530

    ye

--- app/components/common/Footer.jsx
+++ app/components/common/Footer.jsx
@@ -5,7 +5,7 @@ import { Instagram, Github, Youtube, Linkedin } from "lucide-react";
 
 export default function Footer() {
   return (
-    <div className="flex items-center justify-between md:pt-16 md:pr-8 md:pl-8 md:pb-6 pb-4 pt-6">
+    <div className="flex items-center justify-between md:pt-16 md:pr-8 md:pl-8 md:pb-6 pb-4 pt-6 pr-6">
       <div className="flex items-center ">
         <Image
           src="/assets/nav/Shreshta.png"
@@ -29,17 +29,11 @@ export default function Footer() {
         <div className="gooddog md:text-4xl text-l md:p-4 text-[#285F5F]">
           © 2024 MITS. All rights reserved.
         </div>
-        <div className="scale-75 md:scale-100 p-0 md:p-4 text-[#285F5F] gap-10 flex flex-row ">
-          <a href="">
+        <div className="md:scale-100 p-0 md:p-4 text-[#285F5F] gap-10 flex flex-row ">
+          <a href="https://www.instagram.com/shreshtamits/">
             <Instagram />
           </a>
-          <a href="">
-          <Linkedin />
-          </a>
-          <a href="">
-            <Youtube />
-          </a>
-          <a href="">
+          <a href="https://github.com/MitsMediaClub/Sreshta-24">
             <Github />
           </a>
         </div>
