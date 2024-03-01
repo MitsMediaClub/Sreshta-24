880e1548096a7b0baa999889e4de0b4ff3fe7823
Rohit George <code.mastermind.07@gmail.com>
2024-03-01 10:09:21 +0530

    Final Ultra Pro Max

--- app/components/arts/LandingNavbar.jsx
+++ app/components/arts/LandingNavbar.jsx
@@ -63,7 +63,7 @@ const LandingNavbar = () => {
             }}
             className="hover:bg-[#375e5edd] py-[0.4vw] px-[5vw] rounded-[30px] cursor-pointer hover:font-bold transition-all ease-in-out duration-300"
           >
-            Home
+            Shreshta
           </span>
           <span
             onClick={() => {
